0ef1d939b2365b6f6f574260ab536e616062c947
fel88 <15663687+fel88@users.noreply.github.com>
2026-05-14 08:08:11 +0300

    minor cleanup

--- OCCPort.OpenGL/Graphic3d_MaterialAspect.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace OCCPort.OpenGL
-{
-    
-
-}
--- OCCPort.OpenGL/Graphic3d_TypeOfPrimitiveArray.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using System;
-
-namespace OCCPort.OpenGL
-{
-    
-}
